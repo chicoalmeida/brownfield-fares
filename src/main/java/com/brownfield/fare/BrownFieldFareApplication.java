e1da979e25fd387c4eb7944948d59080f1ecbfb5
Chico Almeida <francisco.almeida@outlook.com>
2016-09-26 15:36:36 -0300

    creating Fare service

---
+++ src/main/java/com/brownfield/fare/BrownFieldFareApplication.java
@@ -0,0 +1,12 @@
+package com.brownfield.fare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrownFieldFareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrownFieldFareApplication.class, args);
+	}
+}
